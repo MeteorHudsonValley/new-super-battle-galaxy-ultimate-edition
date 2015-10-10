980901a8f71c35334963aa7ede6f4b990b723139
Kevin Newman <Kevin@unFocus.com>
2015-10-10 14:32:08 -0400

    got basic isBusy working for current users who is challenging another

--- client/templates/menu.js
+++ client/templates/menu.js
@@ -3,23 +3,24 @@ Template.Menu.events({
 		var target = $(ev.currentTarget).find('.js-challenge');
 		$('.js-challenge').not(target).removeClass('is-visible');
 		target.toggleClass('is-visible');
-	}
+	},
+  'click .js-challenge-yes': function(evt) {
+    evt.preventDefault();
+    Meteor.users.update( { _id: Meteor.userId() },
+      { $set: { "profile.isBusy": true }} );
+  },
 });
 
 Template.Menu.helpers({
   players () {
     Meteor.subscribe("online_users");
-    var players = Meteor.users.find({
-      "status.online": true,
-      _id: { $ne: Meteor.userId() }
-    });
-    console.log(players.fetch());
+    var players = Meteor.users.find({ "status.online": true, _id: { $ne: Meteor.userId() } });
     return players;
   },
 
   // context - individual users
   isBusy () {
-    return Meteor.user().profile.isBusy;
+    return this.profile && this.profile.isBusy;
   },
 
 });
