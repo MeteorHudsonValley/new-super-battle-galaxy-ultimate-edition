a1074ff808ff02e3f7b0e440b7861717a59d06c9
Kevin Newman <Kevin@unFocus.com>
2015-10-10 13:49:17 -0400

    cleanups and user list integration

--- client/templates/menu.js
+++ client/templates/menu.js
@@ -3,3 +3,21 @@ Template.Menu.events({
 		$(this).children('.js-challenge').toggleClass('is-visible');
 	}
 });
+
+Template.Menu.helpers({
+  players () {
+    Meteor.subscribe("online_users");
+    var players = Meteor.users.find({
+      "status.online": true,
+      _id: { $ne: Meteor.userId() }
+    });
+    console.log(players.fetch());
+    return players;
+  },
+
+  // context - individual users
+  isBusy () {
+    return Meteor.user().profile.isBusy;
+  },
+
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,3 @@
 Meteor.publish("online_users", function() {
-  return Meteor.users.find({ "status.online": true }, {fields: {'isBusy': 1}});
+  return Meteor.users.find({ "status.online": true });
 });
